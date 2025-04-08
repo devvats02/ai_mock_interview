b1c6557160a7e8a129cadeeecd7525a626eb32a8
devvats02 <dev6398360269srgc@gmail.com>
2025-04-08 19:10:20 +0530

    Initial commit

---
+++ firebase/client.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApp , getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; 
+const firebaseConfig = {
+  apiKey: "AIzaSyB37Kq5_FSGNZAEkSRHlggWlO2AqivhOVQ",
+  authDomain: "prepwise-1668d.firebaseapp.com",
+  projectId: "prepwise-1668d",
+  storageBucket: "prepwise-1668d.firebasestorage.app",
+  messagingSenderId: "838332642930",
+  appId: "1:838332642930:web:df073f01cb33fdc8e5b4eb",
+  measurementId: "G-XB5C5D49HH"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
